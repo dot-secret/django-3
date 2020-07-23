0848911822455fc1d27b00b5ec4eaed77989efb5
Redha Noormansyah <redhanoormansyah30@gmail.com>
2020-07-24 00:27:36 +0700

    Push Templating CRM

---
+++ crm1/accounts/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'accounts/dashboard.html')
+def product(request):
+    return render(request, 'accounts/products.html')
+
+
+def customer(request):
+    return render(request, 'accounts/customer.html')
