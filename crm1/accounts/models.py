8e7e734d2a0f8fccde6106e733b85bdee6203364
Redha Noormansyah <redhanoormansyah30@gmail.com>
2020-07-26 23:55:35 +0700

    Commit Readme

--- crm1/accounts/models.py
+++ crm1/accounts/models.py
@@ -32,8 +32,6 @@ class Product(models.Model):
         return self.name
 
 
-
-
 class Order(models.Model):
     STATUS = (
         ('Pending', 'Pending'),
@@ -44,6 +42,9 @@ class Order(models.Model):
     product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
+
+    def str(self):
+        return self.customer
 
 
 
